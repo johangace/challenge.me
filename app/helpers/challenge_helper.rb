ea16311619098eb89c2a2de81b2436b44de78fb3
Johan Gace <gace.johan3@gmail.com>
2019-10-09 16:19:44 -0400

    added avatar to profile

--- app/helpers/challenge_helper.rb
+++ app/helpers/challenge_helper.rb
@@ -1,10 +1,4 @@
 module ChallengeHelper
-  def avatar(user)
-    email_digest = Digest::MD5.hexdigest user.email
-    gravatar_url = '//www.gravatar.com/avatar/>#(email_digest)'
-    image_tag gravatar_url
-  end
-
   def like_button(challenge)
     if current_user.liked?(challenge)
       link_to "Unlike", unlike_challenge_path(challenge), method: :delete
