d8a8a2997ef0ec9bb6d922c3901578d79ada5fdd
johan gace <gace.johan3@gmail.com>
2019-11-16 17:53:10 -0500

    refactor challenge form

--- app/helpers/challenge_helper.rb
+++ app/helpers/challenge_helper.rb
@@ -1,4 +1,13 @@
 module ChallengeHelper
+  
+  def challenge_form_for(content_type)
+    form_for Challenge.new do |form|
+    form.hidden_field(:content_type , value: content_type) +
+    form.fields_for( :content ) {  |content_form| yield(content_form) } +
+    form.submit("Challenge" )
+    end 
+  end
+
   def like_button(challenge)
     if current_user.liked?(challenge)
       link_to "Unlike", unlike_challenge_path(challenge), method: :delete
